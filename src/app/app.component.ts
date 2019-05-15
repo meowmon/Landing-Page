90891a5edc37a2a72d9629b3ede31e09f0fd32dd
meowmon <meepo9669@gmail.com>
2019-05-15 12:42:40 +0700

    15/5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -9,62 +8,6 @@ import * as $ from 'jquery';
 export class AppComponent {
   title = 'LandingPage';
   public ngOnInit(){
-    $(function() {
-      var $l = $('#carousel-left'),
-        $c = $('#carousel-center'),
-        $r = $('#carousel-right');
-        
-      $l.carouFredSel({
-        auto: false,
-        items: 1,
-        direction: 'right',
-        prev: {
-          button: '#prev',
-          fx: 'uncover',
-          onBefore: function() {
-            setTimeout(function() {
-              $c.trigger( 'prev' );
-            }, 100);
-          }
-        },
-        next: {
-          fx: 'cover'
-        }
-      });
-      $c.carouFredSel({
-        auto: false,
-        items: 1,
-        prev: {
-          onBefore: function() {
-            setTimeout(function() {
-              $r.trigger( 'prev' );
-            }, 100);
-          }
-        },
-        next: {
-          onBefore: function() {
-            setTimeout(function() {
-              $l.trigger( 'next' );
-            }, 100);
-          }
-        }
-      });
-      $r.carouFredSel({
-        auto: false,
-        items: 1,
-        prev: {
-          fx: 'cover'
-        },
-        next: {
-          button: '#next',
-          fx: 'uncover',
-          onBefore: function() {
-            setTimeout(function() {
-              $c.trigger( 'next' );
-            }, 100);
-          }
-        }
-      });
-    });
+   
   }
 }
